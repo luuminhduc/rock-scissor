f260fca42063f080566fa7d85675e5698d35680f
Duc Luu <luuminhduc00@gmail.com>
2021-11-07 16:10:49 +0700

    fix message

--- src/App.js
+++ src/App.js
@@ -5,20 +5,20 @@ import Results from './components/Results';
 import Rules from './components/Rules';
 
 const App = () => {
-  const [showRule,setShowRule] = useState(false);
-  const [myChoice,setMyChoice] = useState(null);
-  const [computerChoice,setComputerChoice] = useState(null);
+  const [showRule, setShowRule] = useState(false);
+  const [myChoice, setMyChoice] = useState(null);
+  const [computerChoice, setComputerChoice] = useState(null);
   const [isPlay, setIsPlay] = useState(true);
   const arr = ["rock", "paper", "scissor", "spock", "lizard"];
-  const [computerScore,setComputerScore] = useState(0);
-  const [score,setScore] = useState(0);
-  const [winner,setWinner] = useState("");
-  const [compareText,setCompareText] = useState("");
+  const [computerScore, setComputerScore] = useState(0);
+  const [score, setScore] = useState(0);
+  const [winner, setWinner] = useState("");
+  const [compareText, setCompareText] = useState("");
 
 
   // hanlde click icon
   const iconClick = (item) => {
-    if(isPlay)setMyChoice(item);
+    if (isPlay) setMyChoice(item);
   }
 
   // Get computer choice;
@@ -27,8 +27,8 @@ const App = () => {
   }
 
   const iWin = () => {
-    setCompareText("Kinh đấy");
-    setScore(score+1);
+    setCompareText("You win");
+    setScore(score + 1);
     setWinner("i");
   }
   const even = () => {
@@ -36,8 +36,8 @@ const App = () => {
   }
 
   const computerWin = () => {
-    setCompareText("Ngu như một con chó, dit me may")
-    setComputerScore(computerScore+1);
+    setCompareText("You lost")
+    setComputerScore(computerScore + 1);
     setWinner("computer");
   }
 
@@ -58,76 +58,76 @@ const App = () => {
 
 
   const withRock = () => {
-    switch(computerChoice) {
-      case "scissor": iWin();break;
-      case "paper": computerWin();break;
-      case "lizard":iWin();break;
-      case "spock":computerWin(); break;
+    switch (computerChoice) {
+      case "scissor": iWin(); break;
+      case "paper": computerWin(); break;
+      case "lizard": iWin(); break;
+      case "spock": computerWin(); break;
       default: even();
     }
   }
 
   const withScissor = () => {
-    switch(computerChoice) {
-      case "rock": computerWin();break;
-      case "paper": iWin();break;
-      case "lizard":iWin();break;
-      case "spock":computerWin(); break;
+    switch (computerChoice) {
+      case "rock": computerWin(); break;
+      case "paper": iWin(); break;
+      case "lizard": iWin(); break;
+      case "spock": computerWin(); break;
       default: even();
     }
   }
 
 
   const withPaper = () => {
-    switch(computerChoice) {
-      case "rock": iWin();break;
-      case "scissor": computerWin();break;
-      case "lizard":computerWin();break;
-      case "spock":iWin(); break;
+    switch (computerChoice) {
+      case "rock": iWin(); break;
+      case "scissor": computerWin(); break;
+      case "lizard": computerWin(); break;
+      case "spock": iWin(); break;
       default: even();
     }
   }
 
   const withLizard = () => {
-    switch(computerChoice) {
-      case "rock": computerWin();break;
-      case "scissor": computerWin();break;
-      case "paper":iWin();break;
-      case "spock":iWin(); break;
+    switch (computerChoice) {
+      case "rock": computerWin(); break;
+      case "scissor": computerWin(); break;
+      case "paper": iWin(); break;
+      case "spock": iWin(); break;
       default: even();
     }
   }
 
   const withSpock = () => {
-    switch(computerChoice) {
-      case "rock": iWin();break;
-      case "scissor": iWin();break;
-      case "paper":computerWin();break;
-      case "lizard":computerWin(); break;
+    switch (computerChoice) {
+      case "rock": iWin(); break;
+      case "scissor": iWin(); break;
+      case "paper": computerWin(); break;
+      case "lizard": computerWin(); break;
       default: even();
     }
   }
 
   const compare = () => {
-    if(myChoice === computerChoice) {
+    if (myChoice === computerChoice) {
       even();
-    }else{
-      switch(myChoice) {
+    } else {
+      switch (myChoice) {
         case "rock": withRock();
-        break
-        case "paper":withPaper();
-        break;
-        case "lizard":withLizard();
-        break;
-        case "spock":withSpock();
-        break;
+          break
+        case "paper": withPaper();
+          break;
+        case "lizard": withLizard();
+          break;
+        case "spock": withSpock();
+          break;
         default: withScissor();
       }
     }
   }
 
   useEffect(() => {
-    if(isPlay && myChoice){
+    if (isPlay && myChoice) {
       setTimeout(() => {
         getComputerChoice()
       }, [1500])
@@ -137,7 +137,7 @@ const App = () => {
   }, [myChoice]);
 
   useEffect(() => {
-    if(computerChoice) {
+    if (computerChoice) {
       setIsPlay(false);
       setTimeout(() => {
         compare()
@@ -149,17 +149,17 @@ const App = () => {
 
   return (
     <div className="container">
-      {showRule && <Rules setShowRule={setShowRule}/>}
-      <Results score={score} computerScore={computerScore}/>
+      {showRule && <Rules setShowRule={setShowRule} />}
+      <Results score={score} computerScore={computerScore} />
       {
-        !myChoice ? <PlayingGround arr={arr} iconClick={iconClick}/>: <CompareGround reset={reset} newRound={newRound} winner={winner} compareText={compareText} computerChoice={computerChoice} myChoice={myChoice}/>
+        !myChoice ? <PlayingGround arr={arr} iconClick={iconClick} /> : <CompareGround reset={reset} newRound={newRound} winner={winner} compareText={compareText} computerChoice={computerChoice} myChoice={myChoice} />
       }
       <div className="btn-container">
-      <button onClick={() => setShowRule(true)} className="btn-show-rule">Rules</button>
+        <button onClick={() => setShowRule(true)} className="btn-show-rule">Rules</button>
 
       </div>
     </div>
   );
 }
- 
+
 export default App;
